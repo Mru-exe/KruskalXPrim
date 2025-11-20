8e84dff3404aea7c8a38509f2e89aa0312808544
Martin Kindl <martin@kindl.cz>
2025-11-20 22:15:06 +0100

    Added Prim tests

--- tests/MSTTest.cpp
+++ tests/MSTTest.cpp
@@ -27,3 +27,26 @@ TEST_CASE("MSTTest - Kruskal") {
         }
     }
 }
+
+TEST_CASE("MSTTest - Prim") {
+    Graph g = Graph::getRandomGraph(100, 50);
+
+    SECTION("MST is Minimal (random graph)") {
+        Graph mst;
+        mst = MST::prim(g);
+
+        //For each edge, removing it should increase the number of components
+        for (auto removed : mst.getEdgeList()) {
+            UnionFind uf;
+            for (auto valid : mst.getEdgeList()) {
+                uf.find(valid.a);
+                uf.find(valid.b);
+                if (removed == valid) {
+                    continue;
+                }
+                uf.unite(valid.a, valid.b);
+            }
+            REQUIRE(uf.getSetCount() != 1);
+        }
+    }
+}
