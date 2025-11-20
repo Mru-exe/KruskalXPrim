6eaafcd792736b414de1424f5f8b3dc9a532c8bf
Martin Kindl <martin@kindl.cz>
2025-11-20 21:59:09 +0100

    Added Kruskal tests

---
+++ tests/MSTTest.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "catch.hpp"
+#include "../Graph.hpp"
+#include "../MST.hpp"
+#include "../UnionFind.hpp"
+
+TEST_CASE("MSTTest - Kruskal") {
+    Graph g = Graph::getRandomGraph(100, 50);
+
+    SECTION("MST is Minimal (random graph)") {
+        Graph mst;
+        mst = MST::kruskal(g);
+
+        //For each edge, removing it should increase the number of components
+        for (auto removed : mst.getEdgeList()) {
+            UnionFind uf;
+            for (auto valid : mst.getEdgeList()) {
+                uf.find(valid.a);
+                uf.find(valid.b);
+                if (removed == valid) {
+                    continue;
+                }
+                uf.unite(valid.a, valid.b);
+            }
+            REQUIRE(uf.getSetCount() != 1);
+        }
+    }
+}
